614a39b806a9df5357d08c6adaad2156b81b2546
Kevin.Li <li_zhaoyu@msn.com>
2015-06-21 14:06:24 +0800

    Initial Commit

---
+++ DeepCreation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DeepCreation
+//
+//  Created by Zhaoyu Li on 6/21/15.
+//  Copyright (c) 2015 Zhaoyu Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DeepCreation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DeepCreation
+//
+//  Created by Zhaoyu Li on 6/21/15.
+//  Copyright (c) 2015 Zhaoyu Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
